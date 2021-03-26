1fcddfed934a4a1406d550f64563db382bc33424
josearmandog02 <josearmandog02@gmail.com>
2021-03-26 11:52:32 -0400

    Switch class was added

---
+++ basic-js/switch/switch.js
@@ -0,0 +1,15 @@
+var number = 1;
+
+switch (number) {
+  case 1:
+    console.log("I am one");
+    break;
+  case 10:
+    console.log("I am ten");
+    break;
+  case 100:
+    console.log("I am one hundred");
+    break;
+  default:
+    console.log("I am default");
+}
