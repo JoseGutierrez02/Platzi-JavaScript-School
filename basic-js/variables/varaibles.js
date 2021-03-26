9f84108fdf352bedfa5a3ad591fe9600619a5e3b
josearmandog02 <josearmandog02@gmail.com>
2021-03-26 10:03:08 -0400

    Variables class was added

---
+++ basic-js/variables/varaibles.js
@@ -0,0 +1,16 @@
+var name = "John";
+
+// to declare a variable
+var age;
+// initialize a variable
+age = 23;
+
+
+// to declare and initialize a variable with an array
+var elements = ["Computer", "Cellphone"];
+
+// to declare and initialize a variable with an object
+var person = {
+  name: "John",
+  age: 45,
+}
