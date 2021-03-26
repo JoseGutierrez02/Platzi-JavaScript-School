fd32cc83fd647f09cccdafd51d240406dd1c701e
josearmandog02 <josearmandog02@gmail.com>
2021-03-26 11:22:22 -0400

    Operator class was added

---
+++ basic-js/operators/operators.js
@@ -0,0 +1,39 @@
+// Binary operators
+
+// Operation
+
+3 + 2
+50 - 10
+10 * 3
+20 / 2
+
+'Jose ' + 'Gutierrez' // concatenation
+
+// Unary operator
+
+// Logic 
+!false // not operator (no)
+
+// Assignation 
+var a = 1; 
+
+// Comparative 
+
+3 == '3' // equal
+
+3 === '3' // strictly equal
+
+5 < 3, 5 <= 3, 5 > 3, 5 >= 3 // less than, less or equal than, greater, greater or equal 
+
+a && b // and operator - both condition bust be true 
+
+true || false // or operator - one of the conditions must be true
+
+// Operator that can be used only on variables 
+
+var edad = 40;
+
+edad++;  // increment by 1
+edad += 2; // increment by 2
+
+edad;
