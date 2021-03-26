82ae0194ad263f9cde4129d9fae2fd32cba51518
josearmandog02 <josearmandog02@gmail.com>
2021-03-26 18:17:14 -0400

    Object constructor class was added

---
+++ basic-js/objects/constructingObjects.js
@@ -0,0 +1,9 @@
+function car(brand, model, year) {
+  this.brand = brand,
+  this.model = model,
+  this.year = year
+}
+
+var newCar = new car('Toyota', 'Corolla', 2020);
+console.log(newCar.model);
+
