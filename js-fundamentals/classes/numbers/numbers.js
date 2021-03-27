32d1dea32835ce9297104f109cb0b4aa2af5cdd5
josearmandog02 <josearmandog02@gmail.com>
2021-03-27 10:53:01 -0400

    Numbers class was added

---
+++ js-fundamentals/classes/numbers/numbers.js
@@ -0,0 +1,27 @@
+var age = 23;
+
+// age = age + 1
+age += 1;
+
+var weight = 75;
+
+// weight = weight - 2
+weight -= 2;
+
+var sandwich = 1;
+
+weight += sandwich;
+
+var playSoccer = 3;
+
+weight -= playSoccer;
+
+winePrice = 200.3;
+
+var total = Math.round(winePrice * 100 * 3) / 100;
+var totalStr = total.toFixed(3);
+var total2 = parseFloat(totalStr);
+
+var pizza = 8;
+var people = 2;
+var slicesPerPerson = pizza / people;
