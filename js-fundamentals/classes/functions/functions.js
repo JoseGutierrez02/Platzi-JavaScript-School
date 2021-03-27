411e035efcf198ad310b954509f8a48060b59319
josearmandog02 <josearmandog02@gmail.com>
2021-03-27 11:02:34 -0400

    Functions class was added

---
+++ js-fundamentals/classes/functions/functions.js
@@ -0,0 +1,9 @@
+var firstName = 'Jose', age = 23;
+
+function printAge(n, a) {
+  console.log(`${n} is ${a} years old`);
+}
+
+printAge(firstName, age);
+printAge('Selena', 24);
+printAge('Daniel', 17);
