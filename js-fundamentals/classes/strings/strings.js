7131f4358f3299a02c58e751cfc371a902216617
josearmandog02 <josearmandog02@gmail.com>
2021-03-27 10:01:46 -0400

    Strings class was added

---
+++ js-fundamentals/classes/strings/strings.js
@@ -0,0 +1,11 @@
+var firstName = 'Jose', lastName = 'Gutierrez';
+
+var nameInUpperCase = firstName.toUpperCase();
+var lastNameInLowerCase = lastName.toLocaleLowerCase();
+
+var firstLetterOfName = firstName.charAt(0);
+var LetterCountOfName = firstName.length;
+
+var fullName = `${firstName} ${lastName.toUpperCase()}`;
+
+var str = firstName.substr(1, 2);
