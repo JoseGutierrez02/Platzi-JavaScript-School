312e4051b9c9cb26a80c60b2bbf728fe5b366f8f
josearmandog02 <josearmandog02@gmail.com>
2021-03-27 10:33:46 -0400

    Variables class was added

---
+++ js-fundamentals/classes/variables/variables.js
@@ -0,0 +1,7 @@
+var firstName = 'Jose', lastName = 'Gutierrez';
+var age = '23 years old';
+
+console.log('Hello, I am ' + firstName + ' ' + lastName);
+console.log('And I am ' + age);
+
+var weight = 75;
