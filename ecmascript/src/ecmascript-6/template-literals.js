fe3b221e3be4fcbce23cc5550246c95b7f1f6b04
JoseGutierrez02 <josearmandog02@gmail.com>
2021-04-02 09:03:32 -0400

    ES6: template literals class was added

---
+++ ecmascript/src/ecmascript-6/template-literals.js
@@ -0,0 +1,9 @@
+// Before
+let hello = 'Hello';
+let world = 'World';
+let epicPhrase = hello + ' ' + world;
+console.log(epicPhrase);
+
+// ES6
+let epicPhrase2 = `${hello} ${world}`;
+console.log(epicPhrase2);
