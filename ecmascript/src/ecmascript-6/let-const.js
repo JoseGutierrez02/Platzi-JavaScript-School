1e3ab5903a7800ce9d32723acb68622155bd8447
JoseGutierrez02 <josearmandog02@gmail.com>
2021-04-02 09:20:31 -0400

    ES6: let and const class was added

---
+++ ecmascript/src/ecmascript-6/let-const.js
@@ -0,0 +1,16 @@
+// ES6
+// let
+{
+  var globalVar = 'Global Var';
+}
+
+{
+  let globalLet = 'Global Let';
+  console.log(globalLet);
+}
+
+console.log(globalVar);
+
+// const
+const a = 'b';
+a = 'a';
