a19b93478794e4cce590dbcb311e940932692177
JoseGutierrez02 <josearmandog02@gmail.com>
2021-04-02 11:12:20 -0400

    ES10: trim class was added

---
+++ ecmascript/src/ecmascript-10/trim.js
@@ -0,0 +1,8 @@
+// ES10
+let hello = '                 hello world';
+console.log(hello);
+console.log(hello.trimStart());
+
+let helloWorld = 'hello world         ';
+console.log(helloWorld);
+console.log(helloWorld.trimEnd());
