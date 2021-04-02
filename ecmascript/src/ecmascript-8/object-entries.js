1dd3b7b62306705bf4b4722502dacb6b67a0f0d9
JoseGutierrez02 <josearmandog02@gmail.com>
2021-04-02 10:24:52 -0400

    ES7: object entries class was added

---
+++ ecmascript/src/ecmascript-8/object-entries.js
@@ -0,0 +1,10 @@
+// ES7
+const data = {
+  frontend: 'Jose',
+  backend: 'Jesus',
+  design: 'Selena'
+};
+
+const entries = Object.entries(data);
+console.log(entries);
+console.log(entries.length);
