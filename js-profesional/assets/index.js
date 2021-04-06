75f22be46ecd823b8f213d0f2f79cf58e4af7aef
JoseGutierrez02 <josearmandog02@gmail.com>
2021-04-06 09:32:21 -0400

    MediaPlayer: getters and setters were added

--- js-profesional/assets/index.js
+++ js-profesional/assets/index.js
@@ -5,6 +5,8 @@ const video = document.querySelector('video');
 const playButton = document.querySelector('#playButton');
 const muteButton = document.querySelector('#muteButton');
 
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+const player = new MediaPlayer({ el: video, plugins: [
+  new AutoPlay(),
+]});
 playButton.onclick = () => player.togglePlay();
 muteButton.onclick = () => player.toggleMute();
