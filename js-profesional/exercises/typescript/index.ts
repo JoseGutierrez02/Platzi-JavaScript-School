d8de44df12aa0206d95542d304438a7d6de48c3a
JoseGutierrez02 <josearmandog02@gmail.com>
2021-04-07 09:49:18 -0400

    TypeScript: functions class was added

--- js-profesional/exercises/typescript/index.ts
+++ js-profesional/exercises/typescript/index.ts
@@ -40,3 +40,26 @@ wildcard = { type: 'Wildcard' };
 
 // Object
 let someObject: object = { type: 'Wildcard' };
+
+// Functions
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+const sum = add(4, 6);
+
+function createAdder(a: number): (number) => number {
+  return function(b: number) {
+    return b + a;
+  }
+}
+
+const addFour = createAdder(4);
+const fourPlusSix = addFour(6);
+
+function fullName(firstName: string, lastName: string = 'Smith'): string {
+  return `${firstName} ${lastName}`;
+}
+
+const jose = fullName('Agent');
+console.log(jose);
