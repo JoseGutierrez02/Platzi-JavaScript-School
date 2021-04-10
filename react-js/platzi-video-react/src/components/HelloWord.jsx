43b099e001c1770c928ea5af95ebc6cd889751b2
JoseGutierrez02 <josearmandog02@gmail.com>
2021-04-10 18:39:17 -0400

    PlatziVideo: initialsetup was added

---
+++ react-js/platzi-video-react/src/components/HelloWord.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+<<<<<<< HEAD
+import '../assets/styles/App.scss';
+
+const HelloWorld = () => (
+  <h1>Hello World</h1>
+);
+=======
+
+const HelloWorld = () => {
+  <h1>Hello World</h1>
+};
+>>>>>>> c1ec5238dec9687c3d4b2d6d0a108a67c30dd26b
+
+export default HelloWorld;
