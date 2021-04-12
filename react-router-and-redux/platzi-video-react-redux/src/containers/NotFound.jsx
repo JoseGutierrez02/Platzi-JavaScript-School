aa02fb56c698a7be2d7d9eddbd5fe0aff8e44bef
JoseGutierrez02 <josearmandog02@gmail.com>
2021-04-12 12:00:58 -0400

    Not found container was added

---
+++ react-router-and-redux/platzi-video-react-redux/src/containers/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/styles/components/NotFound.scss';
+
+const NotFound = () => (
+  <section className='not-found'>
+    <div className='not-found__container'>
+      <h1 className='animated pulse'>404</h1>
+      <p>Página no encontrada</p>
+    </div>
+  </section>
+);
+
+export default NotFound;
