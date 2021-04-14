8d84e218e7911fa4f8acad9de30d347fbf4ef844
JoseGutierrez02 <josearmandog02@gmail.com>
2021-04-14 10:34:04 -0400

    Register functionality was added

--- react-router-and-redux/platzi-video-react-redux/src/containers/Register.jsx
+++ react-router-and-redux/platzi-video-react-redux/src/containers/Register.jsx
@@ -1,25 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { registerRequest } from '../actions';
 import '../assets/styles/components/Register.scss';
 
-const Register = () => (
-  <section className='register'>
-    <section className='register__container'>
-      <h2>Regístrate</h2>
-      <form className='register__container--form'>
-        <input aria-label='Nombre' className='input' type='text' placeholder='Nombre' />
-        <input aria-label='Correo' className='input' type='text' placeholder='Correo' />
-        <input aria-label='Contaseña' className='input' type='password' placeholder='Contraseña' />
-        <button className='button'>Registrarme</button>
-      </form>
-      <p className='register__container--login'>
-        Ya tienes cuenta? 
-        <Link to='/login'>
-          Inicia sesión
-        </Link>
-      </p>
-    </section>
-  </section>
-);
+const Register = (props) => {
+  const [form, setValues] = useState({
+    email: '',
+    name: '',
+    password: '',
+  });
 
-export default Register;
+  const handleInput = (event) => {
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.registerRequest(form);
+    props.history.push('/');
+  };
+  return (
+    <section className='register'>
+      <section className='register__container'>
+        <h2>Regístrate</h2>
+        <form className='register__container--form' onSubmit={handleSubmit}>
+          <input 
+            name='name'
+            aria-label='Nombre' 
+            className='input' 
+            type='text' 
+            placeholder='Nombre'
+            onChange={handleInput}
+          />
+          <input
+            name='email' 
+            aria-label='Correo'
+            className='input' 
+            type='text' 
+            placeholder='Correo'
+            onChange={handleInput}
+          />
+          <input
+            name='password'
+            aria-label='Contaseña' 
+            className='input' 
+            type='password' 
+            placeholder='Contraseña'
+            onChange={handleInput}
+          />
+          <button className='button'>Registrarme</button>
+        </form>
+        <p className='register__container--login'>
+          Ya tienes cuenta? 
+          <Link to='/login'>
+            Inicia sesión
+          </Link>
+        </p>
+      </section>
+    </section>
+  );
+};
+
+const mapDispatchToProps = {
+  registerRequest,
+};
+
+export default connect(null, mapDispatchToProps)(Register);
