c66036520c7b97f2b964c29a576b8a94c8eddc82
JoseGutierrez02 <josearmandog02@gmail.com>
2021-04-16 18:33:18 -0400

    Native c++ modules from node, class was added

---
+++ fundamentos-node-js/modules/native/build/Release/.deps/Release/obj.target/addon/hello.o.d
@@ -0,0 +1,21 @@
+cmd_Release/obj.target/addon/hello.o := g++ -o Release/obj.target/addon/hello.o ../hello.cc '-DNODE_GYP_MODULE_NAME=addon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D__STDC_FORMAT_MACROS' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DBUILDING_NODE_EXTENSION' -I/home/josearmandog02/.cache/node-gyp/14.15.5/include/node -I/home/josearmandog02/.cache/node-gyp/14.15.5/src -I/home/josearmandog02/.cache/node-gyp/14.15.5/deps/openssl/config -I/home/josearmandog02/.cache/node-gyp/14.15.5/deps/openssl/openssl/include -I/home/josearmandog02/.cache/node-gyp/14.15.5/deps/uv/include -I/home/josearmandog02/.cache/node-gyp/14.15.5/deps/zlib -I/home/josearmandog02/.cache/node-gyp/14.15.5/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/addon/hello.o.d.raw   -c
+Release/obj.target/addon/hello.o: ../hello.cc \
+ /home/josearmandog02/.cache/node-gyp/14.15.5/include/node/node.h \
+ /home/josearmandog02/.cache/node-gyp/14.15.5/include/node/v8.h \
+ /home/josearmandog02/.cache/node-gyp/14.15.5/include/node/cppgc/common.h \
+ /home/josearmandog02/.cache/node-gyp/14.15.5/include/node/v8config.h \
+ /home/josearmandog02/.cache/node-gyp/14.15.5/include/node/v8-internal.h \
+ /home/josearmandog02/.cache/node-gyp/14.15.5/include/node/v8-version.h \
+ /home/josearmandog02/.cache/node-gyp/14.15.5/include/node/v8config.h \
+ /home/josearmandog02/.cache/node-gyp/14.15.5/include/node/v8-platform.h \
+ /home/josearmandog02/.cache/node-gyp/14.15.5/include/node/node_version.h
+../hello.cc:
+/home/josearmandog02/.cache/node-gyp/14.15.5/include/node/node.h:
+/home/josearmandog02/.cache/node-gyp/14.15.5/include/node/v8.h:
+/home/josearmandog02/.cache/node-gyp/14.15.5/include/node/cppgc/common.h:
+/home/josearmandog02/.cache/node-gyp/14.15.5/include/node/v8config.h:
+/home/josearmandog02/.cache/node-gyp/14.15.5/include/node/v8-internal.h:
+/home/josearmandog02/.cache/node-gyp/14.15.5/include/node/v8-version.h:
+/home/josearmandog02/.cache/node-gyp/14.15.5/include/node/v8config.h:
+/home/josearmandog02/.cache/node-gyp/14.15.5/include/node/v8-platform.h:
+/home/josearmandog02/.cache/node-gyp/14.15.5/include/node/node_version.h:
