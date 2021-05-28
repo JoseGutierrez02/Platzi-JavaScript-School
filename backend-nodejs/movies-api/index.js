9abff90f1c63f5a59e427b7f1cfab10926435d0e
JoseGutierrez02 <josearmandog02@gmail.com>
2021-05-28 10:20:32 -0400

    [project] error handler middleware was added

--- backend-nodejs/movies-api/index.js
+++ backend-nodejs/movies-api/index.js
@@ -4,10 +4,16 @@ const app = express();
 const { config } = require('./config');
 const moviesApi = require('./routes/movies');
 
+const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers');
+
 // body-parser middleware
 app.use(express.json());
 
 moviesApi(app);
+
+// error-handler middleware
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(config.port, () => {
   console.log(`Server listening at http://localhost:${config.port}`);
