b8a0015f9159cbf60fdcf60547a1a4806ad19266
JoseGutierrez02 <josearmandog02@gmail.com>
2021-06-08 12:24:38 -0400

    SSR basic config was added

--- express-ssr/platzi-videos/webpack.config.js
+++ express-ssr/platzi-videos/webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['./src/frontend/index.js', 'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=2000&reload=true'],
+  mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
@@ -57,10 +59,12 @@ module.exports = {
     open: true,
   },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
     new HtmlWebpackPlugin({
       template: './public/index.html',
       filename: './index.html',
-      favicon: './src/assets/static/platzi_favicon.png',
+      favicon: './src/frontend/assets/static/platzi_favicon.png',
     }),
     new MiniCssExtractPlugin({
       filename: 'assets/[name].css',
