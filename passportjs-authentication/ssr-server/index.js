5135d12b6bf4557f26e59d722f44cde60a9f3b47
JoseGutierrez02 <josearmandog02@gmail.com>
2021-08-04 21:30:35 -0400

    ssr server basic config was added

---
+++ passportjs-authentication/ssr-server/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const { config } = require('./config')
+
+const app = express()
+
+// body parser
+app.use(express.json())
+
+app.post('/auth/sign-in', async (req, res, next) => {})
+
+app.post('/auth/sign-up', async (req, res, next) => {})
+
+app.get('/movies', async (req, res, next) => {})
+
+app.post('/user-movies', async (req, res, next) => {})
+
+app.delete('/user-movies/:userMovieId', async (req, res, next) => {})
+
+app.listen(config.port, () => {
+  console.log(`Listening http://localhost:${config.port}`)
+})
