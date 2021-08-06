2bf7c45fe8561d389c69632720e27c7475e9390c
JoseGutierrez02 <josearmandog02@gmail.com>
2021-08-05 21:38:46 -0400

    ssr sign-in with cookies and sign-up routes were added

--- passportjs-authentication/ssr-server/index.js
+++ passportjs-authentication/ssr-server/index.js
@@ -1,22 +1,69 @@
-const express = require('express')
+const express = require('express');
+const passport = require('passport');
+const boom = require('@hapi/boom');
+const cookieParser = require('cookie-parser');
+const axios = require('axios');
+const { config } = require('./config');
 
-const { config } = require('./config')
-
-const app = express()
+const app = express();
 
 // body parser
-app.use(express.json())
+app.use(express.json());
+// cookie parser
+app.use(cookieParser());
 
-app.post('/auth/sign-in', async (req, res, next) => {})
+// basic strategy
+require('./utils/auth/strategies/basic');
 
-app.post('/auth/sign-up', async (req, res, next) => {})
+app.post('/auth/sign-in', async (req, res, next) => {
+  passport.authenticate('basic', (error, data) => {
+    try {
+      if (error || !data) {
+        next(boom.unauthorized());
+      }
 
-app.get('/movies', async (req, res, next) => {})
+      req.login(data, { session: false }, async (error) => {
+        if (error) {
+          next(error);
+        }
 
-app.post('/user-movies', async (req, res, next) => {})
+        const { token, ...user } = data;
 
-app.delete('/user-movies/:userMovieId', async (req, res, next) => {})
+        res.cookie('token', token, {
+          httpOnly: !config.dev,
+          secure: !config.dev,
+        });
+
+        res.status(200).json(user);
+      });
+    } catch (error) {
+      next(error);
+    }
+  })(req, res, next);
+});
+
+app.post('/auth/sign-up', async (req, res, next) => {
+  const { body: user } = req;
+
+  try {
+    axios({
+      url: `${config.apiUrl}/api/auth/sign-up`,
+      method: 'post',
+      data: user,
+    });
+
+    res.status(201).json({ messaage: 'user created' });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get('/movies', async (req, res, next) => {});
+
+app.post('/user-movies', async (req, res, next) => {});
+
+app.delete('/user-movies/:userMovieId', async (req, res, next) => {});
 
 app.listen(config.port, () => {
-  console.log(`Listening http://localhost:${config.port}`)
-})
+  console.log(`Listening http://localhost:${config.port}`);
+});
