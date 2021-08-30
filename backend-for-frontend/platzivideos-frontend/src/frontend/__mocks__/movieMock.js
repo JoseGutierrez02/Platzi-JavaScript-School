a41aefa4ced40feb2dfc6957389b348682d8458b
JoseGutierrez02 <josearmandog02@gmail.com>
2021-08-30 17:19:18 -0400

    [test] setFavorite and loginRequest actions tests were added

---
+++ backend-for-frontend/platzivideos-frontend/src/frontend/__mocks__/movieMock.js
@@ -0,0 +1,16 @@
+const movieMock = {
+  id: 8,
+  slug: 'tvshow-8',
+  title: 'Stargate Atlantis',
+  type: 'Action',
+  language: 'English',
+  year: 2012,
+  contentRating: '16+',
+  duration: 148,
+  cover: 'http://dummyimage.com/800x600.png/306880/ffffff',
+  description: 'Vestibulum ac est lacinia nisi venenatis tristique',
+  source:
+    'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+};
+
+module.exports = movieMock;
---
+++ backend-for-frontend/platzivideos-frontend/src/frontend/__test__/actions/index.test.js
@@ -0,0 +1,27 @@
+import { setFavorite, loginRequest } from '../../actions';
+import movieMock from '../../__mocks__/movieMock';
+
+describe('Actions', () => {
+  test('Set Favorite', () => {
+    const payload = movieMock;
+    const expectedAction = {
+      type: 'SET_FAVORITE',
+      payload,
+    };
+
+    expect(setFavorite(payload)).toEqual(expectedAction);
+  });
+
+  test('Login Request', () => {
+    const payload = {
+      email: 'test@test.com',
+      password: 'password',
+    };
+    const expectedAction = {
+      type: 'LOGIN_REQUEST',
+      payload,
+    };
+
+    expect(loginRequest(payload)).toEqual(expectedAction);
+  });
+});
